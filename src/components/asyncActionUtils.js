74f94d82d2ca77e0933e5eebc40686544e0a8533
goforitrj <eleveniasjr@gmail.com>
2020-02-11 14:47:28 +1100

    Split UserContext.js to api.js/asyncActionUtils.js/UsersContext.js

---
+++ src/components/asyncActionUtils.js
@@ -0,0 +1,77 @@
+export function createAsyncDispatcher(type, promiseFn) {
+    const SUCCESS = `${type}_SUCCESS`;
+    const ERROR = `${type}_ERROR`;
+
+    async function actionHandler(dispatch, ...rest) {
+        dispatch({ type });
+        try {
+            const data = await promiseFn(...rest);
+            dispatch({
+                type: SUCCESS,
+                data: data
+            });
+        } catch (e) {
+            dispatch({
+                type: ERROR,
+                error: e
+            });
+        }
+    }
+    return actionHandler;
+}
+
+export const initialAsyncState = {
+    loading: false,
+    data: null,
+    error: null
+};
+
+const loadingState = {
+    loading: true,
+    data: null,
+    error: null
+};
+
+const success = data => ({
+    loading: false,
+    data: data,
+    error: null
+});
+
+const error = error => ({
+    loading: false,
+    data: null,
+    error: error
+});
+
+export function createAsyncHandler(type, key) {
+    const SUCCESS = `${type}_SUCCESS`;
+    const ERROR = `${type}_ERROR`;
+
+    function what(state, action) {
+        switch (action.type) {
+            case type:
+                return {
+                    ...state,
+                    [key]: loadingState
+                };
+            case SUCCESS:
+                console.log({
+                    ...state,
+                    [key]: success(action.data)
+                });
+                return {
+                    ...state,
+                    [key]: success(action.data)
+                };
+            case ERROR:
+                return {
+                    ...state,
+                    [key]: error(action.error)
+                };
+            default:
+                throw new Error(`unHandled action type ${type}`);
+        }
+    }
+    return what;
+}
